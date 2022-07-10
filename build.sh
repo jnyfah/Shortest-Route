09878679555072f36ca8c5e67c62b26c4a395be0
jnyfah <jnyfaah@gmail.com>
2022-07-10 13:15:57 +0100

    minor changes

--- build.sh
+++ build.sh
@@ -1,13 +1,3 @@
-rm build/ -rf
-mkdir build
-cd build
-emcmake cmake ..  
+emcmake cmake  
 make
-emcc --bind -o index.js ../src/Dijkstra.cpp --std=c++1z --preload-file ../utils || exit 1
-mv index.js ../web/
-mv index.wasm ../web/
-mv index.data ../web/
-
-
-
-
+emcc --bind -o index.js src/Dijkstra.cpp --std=c++1z --preload-file utils || exit 1
